76a5459c8ec558593135e785489fc8a576dba5f8
S2YHA <pawel.kaminski10@gmail.com>
2024-05-22 21:20:00 +0200

    add basic ui

---
+++ components/Calculator.vue
@@ -0,0 +1,58 @@
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>Kalkulator OC/AC</CardTitle>
+    </CardHeader>
+    <CardContent class="grid gap-6">
+      <div class="grid grid-cols-2 gap-4">
+        <div class="grid gap-2">
+          <Label for="netto">Netto</Label>
+          <Input id="netto" type="number" />
+        </div>
+        <div class="grid gap-2">
+          <Label for="brutto">Brutto</Label>
+          <Input id="brutto" type="number" />
+        </div>
+      </div>
+      <div class="grid gap-2">
+        <Label for="subject">Rok produkcji</Label>
+        <Input id="subject" type="number" />
+      </div>
+      <div class="flex gap-2">
+        <Checkbox id="terms1" />
+        <div class="grid gap-1.5 leading-none">
+          <label
+            for="terms1"
+            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Pakiet Drive+
+          </label>
+        </div>
+      </div>
+      <div class="grid gap-2">
+        <Label for="subject">Raty</Label>
+        <RadioGroup default-value="option-one">
+          <div class="flex items-center space-x-2">
+            <RadioGroupItem id="option-one" value="option-one" />
+            <Label for="option-one">Brak</Label>
+          </div>
+          <div class="flex items-center space-x-2">
+            <RadioGroupItem id="option-two" value="option-two" />
+            <Label for="option-two">2</Label>
+          </div>
+          <div class="flex items-center space-x-2">
+            <RadioGroupItem id="option-three" value="option-three" />
+            <Label for="option-three">4</Label>
+          </div>
+        </RadioGroup>
+      </div>
+      <div class="grid gap-2">
+        <Label class="text-center">3250</Label>
+      </div>
+    </CardContent>
+    <CardFooter class="space-x-2">
+      <Button>Oblicz</Button>
+      <Button variant="outline">Wyczyść</Button>
+    </CardFooter>
+  </Card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <Button>Click me</Button>
+  <div
+    class="col-span-2 grid items-start gap-6 lg:col-span-2 lg:grid-cols-2 xl:col-span-1 xl:grid-cols-1"
+  >
+    <div class="mx-auto">
+      <Calculator />
+    </div>
   </div>
 </template>
